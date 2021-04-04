e954bd0534fa46bc717b2ecbad39d54cd3f7a0bb
Maikel <52623494+maikelps@users.noreply.github.com>
2021-04-04 17:42:53 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -18,69 +18,7 @@ from pandas.tseries.offsets import MonthEnd
 from pandas.tseries.offsets import *
 import numpy as np
 #import base64
-#from preprocessing_covid import general_clean, europe_cleaning
-
-
-##### DEFINING HELPER FUNCTIONS FOR PREPROCESSING #####
-
-def general_clean(df):
-    """
-    Function for a basic cleaning of the raw dataframe
-    """
-
-    
-
-    # Cleaning rows
-    row_cleaner = [ 'Africa', 'Asia', 'Central African Republic', 
-                    'World', 'Europe', 'Oceania', 'South America', 
-                    'European Union', 'Anguilla', 'Bosnia and Herzegovina',
-                    'Brunei', 'Chad', 'Comoros', 'Djibouti', 'Equatorial Guinea',
-                    'Eritrea', 'Eswatini','Gabon', 'Greenland', 'Grenada',
-                    'Lesotho', 'Malawi', 'Micronesia (country)', 'Saint Helena', 
-                    'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines',
-                    'Seychelles', 'Turks and Caicos Islands' ]
-
-    return df[ ~df.location.isin(row_cleaner) ]
-
-def europe_cleaning(df, pop = 5000000):
-    """
-    Function for a cleaning in the european countries 
-    """
-
-    # Doing Basic cleaning:
-    df = general_clean(df)
-
-    # Filtering: 
-    df = df[ df.continent == 'Europe']
-
-    # Keeping values with no NaN values in gdp:
-    df = df[ ~df.gdp_per_capita.isna() ]
-
-    # Keeping population over 5Mill
-    df = df[df.population >= pop]
-
-    # Cleaning negative values
-    df = df[df.new_cases >= 0]
-    df = df[df.new_deaths >= 0]
-
-    # --- Preparing exports ---
-
-    # Cases historical
-    dfc = df[['iso_code', 'location', 'date','new_cases', 'new_deaths', 'new_cases_per_million', 'new_deaths_per_million', 
-            'total_cases', 'total_deaths', 'total_cases_per_million', 'total_deaths_per_million', 
-            'population','gdp_per_capita', 'human_development_index']]
-
-    dfc = dfc.sort_values(by=['location', 'date'])
-
-    # Vaccination:
-    dfvac = df[~df.total_vaccinations.isna()]
-
-    dfvac = dfvac[['iso_code', 'location', 'date', 'total_vaccinations', 'new_vaccinations', 
-                'people_vaccinated_per_hundred', 'population','gdp_per_capita', 'human_development_index']]
-
-    dfvac = dfvac.sort_values(by=['location', 'date'])
-
-    return dfc, dfvac
+from preprocessing_covid import europe_cleaning
 
 
 # Importing Preprocessed Data
@@ -129,12 +67,6 @@ time_tosee = [
     {'label': 'Monthly', 'value': 'Monthly'}
 ]
 
-# Image file:
-#image_filename = 'nova_ims.png'
-#encoded_image = base64.b64encode(open(image_filename, 'rb').read())
-
-# The app itself
-
 app = dash.Dash(__name__, external_stylesheets='')
 server = app.server
 
@@ -147,7 +79,6 @@ app.layout = html.Div([
 
         html.Div([
 
-            #html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()), style={'width': '150px'}),
             html.Img(src='https://www.novaims.unl.pt/images/logo.png', style={'width': '130px'}),
 
         ], style={'width': '10%'}, className='ims-image'),
@@ -307,7 +238,6 @@ app.layout = html.Div([
         ])
     ], id = 'cases-vaccination', value = 'Cases', className='tabsss'),
 
-    #html.Footer('Maikel Sousa m20200735 | Data refreshed live from: ',  id = 'footer'), 
     html.Label(['Author: Maikel Sousa m20200735 | Data Source: ', html.A('Our World in Data', href='https://ourworldindata.org/coronavirus-source-data')], className='footer_link'),
 ])
 
